0dd14a5299a04428a51f5bbd6a969337041e2bde
Jahanzaib-Tahir <jahanzeb.mughal.94@gmail.com>
2019-08-08 12:10:31 +0500

    Initial Project Add

---
+++ src/main/java/guru/springframework/dfgpetclinic/DfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.dfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfgPetClinicApplication.class, args);
+    }
+
+}
